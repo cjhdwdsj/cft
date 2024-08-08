191e38aa37754371cfc46a62b684e276c4785036
cjhdwdsj <62288950+cjhdwdsj@users.noreply.github.com>
2024-08-08 16:11:38 +0800

    正式版

--- data/TEST/main.js
+++ data/TEST/main.js
@@ -1,4 +1,4 @@
 document.title = "屯溪一中2024届14班蹭饭图";
 $("#class_name").text("屯溪一中2024届14班")
 $("#groupimg").attr("src", "data/TEST/Group.jpg")
-$("#add_data").attr("href", "https://bilibili.com")
+$("#add_data").attr("href", "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__SC8Q4xURUVEMjNVUjJCNFlUTUJETVE4ODcwSTkwUy4u")
